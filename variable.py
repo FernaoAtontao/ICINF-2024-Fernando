7dbb0f4079768900c5dc1f2f02ec22efc96475c2
FernaoAtontao <168034603+FernaoAtontao@users.noreply.github.com>
2024-05-03 10:02:01 -0400

    si

---
+++ variable.py
@@ -0,0 +1,10 @@
+"""Este es un 
+comentario multilineas"""
+
+#Impresion variable clasica
+name = "Fernando"
+print("Hola soy",name)
+
+#Print clasico
+edad = 19
+print("Hola mi nombre es",name,"y tengo",edad)
