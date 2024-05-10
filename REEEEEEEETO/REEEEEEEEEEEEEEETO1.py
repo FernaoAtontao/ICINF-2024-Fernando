3f0e498d4508691bc512c69b1b6aaef45b557ff0
FernaoAtontao <168034603+FernaoAtontao@users.noreply.github.com>
2024-05-10 08:45:24 -0400

    aaaaaaaaaaa

---
+++ REEEEEEEETO/REEEEEEEEEEEEEEETO1.py
@@ -0,0 +1,9 @@
+#profe porfa publique su codigo como referncia 
+
+nombre = input("¿Cuál es tu nombre? ")
+
+edad = int(input("¿Cuál es tu edad? "))
+
+total_edad = edad + 20
+
+print(f"Hola mi nombre es {nombre} y tengo", edad,"años y en 20 años tendre", total_edad,"años")
