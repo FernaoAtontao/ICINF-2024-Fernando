e1b63b57a60bd3282eb1632349a29fd7655911ad
FernaoAtontao <168034603+FernaoAtontao@users.noreply.github.com>
2024-05-24 09:43:17 -0400

    Update and rename REEEEEEEEEEEEEEETO1.py to Reto.py

---
+++ REEEEEEEETO/Reto.py
@@ -0,0 +1,7 @@
+nombre = input("¿Cuál es tu nombre? ")
+
+edad = int(input("¿Cuál es tu edad? "))
+
+total_edad = edad + 20
+
+print(f"Hola mi nombre es {nombre} y tengo", edad,"años y en 20 años tendre", total_edad,"años")
